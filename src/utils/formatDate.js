d1080aa49e7c28cf51bd8f03ac85276b177f5f96
ujjwal_aggrawal-admin <ujjwal@sanskartechnolab.com>
2025-09-14 21:55:45 +0530

    format Date updated

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,7 +1,7 @@
 export const getTimeFromISTString = (iso) => {
   if (!iso) return "-";
-  const dateObj = new Date(iso);
-  const hours = dateObj.getUTCHours().toString().padStart(2, "0");
-  const minutes = dateObj.getUTCMinutes().toString().padStart(2, "0");
+  const d = new Date(iso);
+  const hours = d.getUTCHours().toString().padStart(2, "0");
+  const minutes = d.getUTCMinutes().toString().padStart(2, "0");
   return `${hours}:${minutes}`;
 };
