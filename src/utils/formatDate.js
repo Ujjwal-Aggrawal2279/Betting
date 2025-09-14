4111f3282b5f2ca32e692bff2e863a78d635851e
ujjwal_aggrawal-admin <ujjwal@sanskartechnolab.com>
2025-09-14 21:51:15 +0530

    time function updated

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,7 +1,7 @@
 export const getTimeFromISTString = (iso) => {
   if (!iso) return "-";
-  const localDate = new Date(iso);
-  const hours = localDate.getHours().toString().padStart(2, "0");
-  const minutes = localDate.getMinutes().toString().padStart(2, "0");
+  const dateObj = new Date(iso);
+  const hours = dateObj.getUTCHours().toString().padStart(2, "0");
+  const minutes = dateObj.getUTCMinutes().toString().padStart(2, "0");
   return `${hours}:${minutes}`;
 };
